ac49666da4541b501cd2901aacae578c40eb09be
Josh Lippiner <jlippiner@noctivity.com>
2012-02-05 20:42:56 -0500

    initial 3.2 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noc::Application.config.secret_token = 'a72c1a7826c3f9dafab365f3ae40254162fa7b7aa3efd347e71ce8de14f878ebfdd10fb010f7a2d0c1f808e78d7d9293db8e656ab41bffb800c96bb68e0443c8'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Noc::Application.routes.draw do
+  get "welcome/index"
+  root :to => 'welcome#index'
+end
